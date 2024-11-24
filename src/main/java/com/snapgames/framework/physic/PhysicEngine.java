c9b3b27fec0fc66f50394cac6620b4ba4b1caf28
Frédéric Delorme <frederic.delorme@gmail.com>
2024-11-24 13:42:13 +0100

    prepare moving to Vector2d
    - Add Entity getters and setters for position, velocity and acceleration.

--- src/main/java/com/snapgames/framework/physic/PhysicEngine.java
+++ src/main/java/com/snapgames/framework/physic/PhysicEngine.java
@@ -28,64 +28,109 @@ import java.util.Optional;
  * @since 1.0.0
  */
 public class PhysicEngine implements GSystem {
+    /**
+     * The Game application instance associated with this PhysicEngine.
+     * This application serves as the main context for running the physics simulations
+     * and managing game states, including the initialization, update loops, and disposal of resources.
+     */
     private final Game app;
+    /**
+     * Represents the current time in milliseconds within the PhysicEngine.
+     */
     private long currentTime = 0;
 
+    /**
+     * Constructs a new PhysicEngine associated with the specified Game application.
+     *
+     * @param app The Game application that this PhysicEngine will be associated with.
+     */
     public PhysicEngine(Game app) {
         this.app = app;
     }
 
+    /**
+     * Updates the state of the scene and its entities based on the elapsed time.
+     * This includes updating the physical states of dynamic entities, keeping entities
+     * within the world boundaries, and updating behaviors for all entities.
+     * Additionally, the active camera is also updated if present.
+     *
+     * @param scene   The scene containing the entities and the world they reside in.
+     * @param elapsed The time elapsed since the last update, in milliseconds.
+     */
     private void update(Scene scene, double elapsed) {
         scene.getEntities().values().stream()
-            .filter(Entity::isActive)
-            .forEach(e -> {
-                if (e.getPhysicType() == PhysicType.DYNAMIC) {
-                    e.setContact(false);
-                    applyWorldEffects(scene, e);
-                    applyPhysicRules(scene, elapsed, e);
-                    keepEntityIntoWorld(scene, e);
-                }
-                e.getBehaviors().forEach(b -> b.update(e, elapsed));
-            });
+                .filter(Entity::isActive)
+                .forEach(e -> {
+                    if (e.getPhysicType() == PhysicType.DYNAMIC) {
+                        e.setContact(false);
+                        applyWorldEffects(scene, e);
+                        applyPhysicRules(scene, elapsed, e);
+                        keepEntityIntoWorld(scene, e);
+                    }
+                    e.getBehaviors().forEach(b -> b.update(e, elapsed));
+                });
         if (Optional.ofNullable(scene.getActiveCamera()).isPresent()) {
             scene.getActiveCamera().update(elapsed);
         }
     }
 
+    /**
+     * Applies the world's physical effects to the specified entity within the scene.
+     *
+     * @param scene The scene containing the world and its children entities.
+     * @param e     The entity to which the world effects are to be applied.
+     */
     private void applyWorldEffects(Scene scene, Entity<?> e) {
         scene.getWorld().getChildren().forEach(a -> {
             if (a.contains(e) || a.intersects(e)) {
                 e.getForces().addAll(((WorldArea) a).getForces());
-                e.dx *= ((WorldArea) a).getMaterial().friction;
-                e.dy *= ((WorldArea) a).getMaterial().friction;
+                e.setVelocity(e.getVelocity().getX() * ((WorldArea) a).getMaterial().friction,
+                        e.getVelocity().getY() * ((WorldArea) a).getMaterial().friction);
                 e.setContact(true);
             }
         });
     }
 
+    /**
+     * Applies the physical rules to a given entity within the specified scene.
+     *
+     * @param scene   The scene containing the world and other entities.
+     * @param elapsed The time elapsed since the last update, in milliseconds.
+     * @param e       The entity to which the physical rules will be applied.
+     */
     private void applyPhysicRules(Scene scene, double elapsed, Entity<?> e) {
-        e.ax = 0;
-        e.ay = 0;
+        e.setAcceleration(0, 0);
         e.addForce(0.0, -scene.getWorld().getGravity() / e.getMass());
         e.getForces().forEach(f -> {
-            e.ax += f.getX();
-            e.ay += f.getY();
+            e.setAcceleration(e.getAcceleration().getX() + f.getX(),
+                    e.getAcceleration().getY() + f.getY());
         });
 
         e.dx += 0.5 * e.ax * elapsed * elapsed * 0.001;
         e.dy += 0.5 * e.ay * elapsed * elapsed * 0.001;
 
-        e.x += e.dx * (elapsed);
-        e.y += e.dy * (elapsed);
+
+        e.setPosition(e.getPosition().getX() + e.getVelocity().getX() * elapsed,
+                e.getPosition().getY() + e.getVelocity().getY() * elapsed);
 
         if (e.hasContact()) {
-            e.dx *= e.getMaterial().friction;
-            e.dy *= e.getMaterial().friction;
+            e.setVelocity(e.getVelocity().getX() * e.getMaterial().friction,
+                    e.getVelocity().getY() * e.getMaterial().friction);
         }
-        e.dx = Math.signum(e.dx) * Math.min(Math.abs(e.dx), 16.0);
-        e.dy = Math.signum(e.dy) * Math.min(Math.abs(e.dy), 16.0);
+        // limit speed to 16.0 max
+        e.setVelocity(Math.signum(e.getVelocity().getX()) * Math.min(Math.abs(e.getVelocity().getX()), 16.0),
+                Math.signum(e.getVelocity().getY()) * Math.min(Math.abs(e.getVelocity().getY()), 16.0)
+        );
     }
 
+    /**
+     * Ensures the given entity remains within the boundaries of the world defined in the scene.
+     * If the entity exceeds the world's boundaries, it will be repositioned and its velocity will be
+     * modified based on its elasticity to simulate a collision response.
+     *
+     * @param scene The scene containing the world and entities.
+     * @param e     The entity to keep within the world boundaries.
+     */
     private void keepEntityIntoWorld(Scene scene, Entity<?> e) {
         World w = scene.getWorld();
         if (!w.contains(e) || w.intersects(e)) {
@@ -112,29 +157,63 @@ public class PhysicEngine implements GSystem {
         }
     }
 
+    /**
+     * Disposes of the resources and performs cleanup tasks associated with the PhysicEngine.
+     */
     public void dispose() {
 
     }
 
+    /**
+     * Resets the forces acting on all entities within the specified scene.
+     *
+     * @param scene The scene containing the entities whose forces need to be reset.
+     */
     public void resetForces(Scene scene) {
         scene.getEntities().values().forEach(e -> e.getForces().clear());
     }
 
+    /**
+     * Retrieves a list of classes that are dependencies for the PhysicEngine.
+     *
+     * @return A list containing the classes Config and SceneManager, which are necessary for the PhysicEngine.
+     */
     @Override
     public List<Class<?>> getDependencies() {
         return List.of(Config.class, SceneManager.class);
     }
 
+    /**
+     * Initializes the PhysicEngine with the specified game instance.
+     *
+     * @param game The game instance with which this PhysicEngine is to be initialized.
+     */
     @Override
     public void initialize(GameInterface game) {
 
     }
 
+    /**
+     * Starts the PhysicEngine with the specified game instance. This method is
+     * invoked to initialize and begin the physics processing within the context
+     * of the provided game instance.
+     *
+     * @param game The game interface instance with which this PhysicEngine is to
+     *             be started.
+     */
     @Override
     public void start(GameInterface game) {
 
     }
 
+    /**
+     * Processes the game state for the given elapsed time and updates the scene if the game is not paused.
+     * This includes managing scenes and integrating game-related statistics.
+     *
+     * @param game   The current game interface instance.
+     * @param elapsed The time elapsed since the last update in milliseconds.
+     * @param stats  A map containing various statistics and metrics related to the game's state.
+     */
     @Override
     public void process(GameInterface game, double elapsed, Map<String, Object> stats) {
         if (game.isNotPaused()) {
@@ -143,17 +222,35 @@ public class PhysicEngine implements GSystem {
         }
     }
 
+    /**
+     * Performs post-processing tasks for the PhysicEngine after the main processing cycle.
+     * This method resets the forces acting on all entities within the active scene of the game.
+     *
+     * @param game The current game interface instance.
+     */
     @Override
     public void postProcess(GameInterface game) {
         SceneManager sm = SystemManager.get(SceneManager.class);
         resetForces(sm.getActiveScene());
     }
 
+    /**
+     * Stops the PhysicEngine and performs necessary cleanup tasks.
+     *
+     * @param game The game interface instance that provides contextual information and control
+     *             for the game application.
+     */
     @Override
     public void stop(GameInterface game) {
 
     }
 
+    /**
+     * Disposes of the resources and performs cleanup tasks associated with the PhysicEngine.
+     *
+     * @param game The game interface instance that provides contextual information and control
+     *             for the game application.
+     */
     @Override
     public void dispose(GameInterface game) {
 
